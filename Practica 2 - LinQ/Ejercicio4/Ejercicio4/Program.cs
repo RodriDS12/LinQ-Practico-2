9d6c97c59349b12f5ee6c069f96bed59ce11c3c5
Rodrigo Denis <114033291+RodriDS12@users.noreply.github.com>
2023-04-14 14:46:35 -0300

    Update Program.cs

--- Practica 2 - LinQ/Ejercicio4/Ejercicio4/Program.cs
+++ Practica 2 - LinQ/Ejercicio4/Ejercicio4/Program.cs
@@ -11,12 +11,16 @@ namespace Ejercicio4
         static void Main(string[] args)
         {
 
+            //i)
+            
             List<int> valores = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             int sumaTotal = valores.Sum();
 
             Console.WriteLine("La suma total es: " + sumaTotal);
 
+            //ii)
+            
             List<int> valores2 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             int sumaTotalValoresPares = (from valor in valores2
